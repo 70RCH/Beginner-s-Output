da840ca9ed677a02c59158db882caa4fbde6f848
70RCH <1121656655@qq.com>
2024-06-19 17:06:25 +1200

    Add files via upload

---
+++ beginner problems 1.py
@@ -0,0 +1,14 @@
+#1
+min = int(input())
+print(min*60,"seconds")
+
+#2
+cards = int(input())
+print ("Jane receives at least",2*cards,"cards")
+
+#3
+age = int(input("Enter your age in years:"))
+print(f"you are {365*age} days old.")
+#3.1
+age1 = int(input("Enter your age in years:"))
+print (f"you are {365*age1} days old. In {36500-365*age1} days you will be 100 years old!")
